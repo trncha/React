c38728c422aeba3de18a121a4ce9dde63d45c63c
Top Teranai <topteranai@Tops-MacBook-Pro.local>
2023-03-19 16:49:47 +0700

    add project accordion

---
+++ accordion-app/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import data from './data';
+import { useState } from 'react'
+import SingleContent from './components/SingleContent';
+
+function App() {
+  const [content, setContent] = useState(data)
+  return (
+    <main>
+      <div className="container">
+        <h3>เครื่องมือพัฒนาเว็บปี 2021</h3>
+        <section>
+          {content.map((data) => {
+            return <SingleContent key={data.id} {...data}/>
+          })}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
